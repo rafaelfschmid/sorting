bcb63c86830faaa7a03614e5bbc1599408879b71
Rafael Schmid <rafaelfschmid@gmail.com>
2016-05-25 23:11:46 -0300

    mergemgpu file and mergethrust file

--- src/scripts/execall.sh
+++ src/scripts/execall.sh
@@ -1,8 +1,15 @@
-./scripts/exec.sh sorts/bitonic/bitonicsort.exe ~/inputs/sorts > times/bitonicsort.time
-./scripts/exec.sh sorts/mergesort/mergesort.exe ~/inputs/sorts > times/mergesort.time
-./scripts/exec.sh sorts/oddevensort/oddevensort.exe ~/inputs/sorts > times/oddevensort.time
-./scripts/exec.sh sorts/quicksort/quicksort ~/inputs/sorts > times/quicksort.time
-./scripts/exec.sh radixcub.exe ~/inputs/sorts > times/radixcub.time
-./scripts/exec.sh radixthrust.exe ~/inputs/sorts > times/radixthrust.time
+dir1=$1
+dir2=$2
+
+#./scripts/exec.sh sorts/bitonic/bitonicsort.exe $dir1 > $dir2/bitonicsort.time
+#./scripts/exec.sh sorts/mergesort/mergesort.exe $dir1 > $dir2/mergesort.time
+#./scripts/exec.sh sorts/oddevensort/oddevensort.exe $dir1 > $dir2/oddevensort.time
+#./scripts/exec.sh sorts/quicksort/quicksort $dir1 > $dir2/quicksort.time
+#./scripts/exec.sh radixcub.exe $dir1 > $dir2/radixcub.time
+#./scripts/exec.sh radixthrust.exe $dir1 > $dir2/radixthrust.time
+#./scripts/exec.sh mergemgpu.exe $dir1 > $dir2/mergemgpu.time
+./scripts/exec.sh radixthrust_stable.exe $dir1 > $dir2/radixthrust_stable.time
+
+
 
 
--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 dir=$1
-c=8
+c=32768
 while [ $c -le 33554432 ] 
 do 	
 	./generate.exe $c > $dir/$c.in
-	sleep 0.5
+	sleep 1.0
 	((c=$c*2))
 done
