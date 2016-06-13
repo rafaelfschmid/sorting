1bf2ddb7ab4dda13f489c2c2fbce11d2dc7dc8d4
Rafael Schmid <rafaelfschmid@gmail.com>
2016-06-12 22:41:31 -0300

    sort times

--- src/scripts/radixtest.sh
+++ src/scripts/radixtest.sh
@@ -2,7 +2,11 @@
 #./scripts/execall.sh ~/inputs/sorts/rand radixrand
 #./scripts/execall.sh ~/inputs/sorts/asc radixasc
 #./scripts/execall.sh ~/inputs/sorts/desc radixdesc
+#./scripts/execall.sh ~/inputs/sorts/rand-18 radixrand-18
+#./scripts/execall.sh ~/inputs/sorts/rand-6 radixrand-6
 
-./scripts/execall.sh ~/inputs/sorts/rand-18 radixrand-18
-./scripts/execall.sh ~/inputs/sorts/rand-6 radixrand-6
+./scripts/execall.sh ~/inputs/sorts/asc-18 radixasc-18
+./scripts/execall.sh ~/inputs/sorts/desc-18 radixdesc-18
+./scripts/execall.sh ~/inputs/sorts/asc-6 radixasc-6
+./scripts/execall.sh ~/inputs/sorts/desc-6 radixdesc-6
 
